9d48bcad30fbc21f5f7a09ea875663312ba7ef83
Alain K <alain.kahwaji@gmail.com>
2015-06-01 15:34:50 -0700

    Add mapview, toolbar, and text field.

---
+++ RoadtripTracker/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  RoadtripTracker
+//
+//  Created by Alain  on 2015-06-01.
+//  Copyright (c) 2015 Alain . All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
